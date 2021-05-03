be33cceb18265a89b86ac89737c3dde84e2d79cd
calebeverett <caleb@calebeverett.io>
2021-05-03 13:11:22 -0700

    revised folder structure

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,9 +4,8 @@ from git import Repo
 
 DOTENV_PATH = "{{cookiecutter.dotenv_path}}"
 INIT_REPO = "{{cookiecutter.init_repo}}" == "yes"
-PUSH_TO_REMOTE = "{{cookiecutter.merge_remote}}" == "yes"
+PUSH_REPO = "{{cookiecutter.push_repo}}" == "yes"
 
-BASE_URL = "https://api.github.com"
 REPO_NAME = "{{cookiecutter.repo_name}}"
 
 if DOTENV_PATH:
@@ -16,8 +15,8 @@ if INIT_REPO:
     repo = Repo.init()
     repo.git.add(all=True)
     repo.index.commit("Add initial project skeleton.")
-
-if PUSH_TO_REMOTE:
     repo.create_remote("origin", url="git@github.com:{{ cookiecutter.repo_name }}.git")
+
+if PUSH_REPO:
     remote = repo.remote("origin")
     remote.push(refspec="master:master")
